f9bf8ed75396d90b7e1f6ffd1563c43c3540f101
DmitriyUshkvarok <dmitriy_ushkvarok@ukr.net>
2023-06-13 21:04:54 +0300

    test

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,9 +4,10 @@ import { Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import authOperation from '../../redux/auth/authOperation';
-import SharedLayout from '../SharedLayout/SharedLayout';
+// import SharedLayout from '../SharedLayout/SharedLayout';
 import Container from '../Container/Container';
 import NoticesCategoriesList from '../Notices/NoticesCategoriesList/NoticesCategoriesList';
+import Header from '../Header/Header';
 
 const HomePage = lazy(() => import('../../pages/HomePage/HomePage'));
 const NewsPage = lazy(() => import('../../pages/NewsPage/NewsPage'));
@@ -35,24 +36,26 @@ function App() {
     <>
       <Suspense fallback={<div>Loading subpage ....</div>}>
         <Container>
+          <Header />
           <Routes>
-            <Route path="/" element={<SharedLayout />}>
-              <Route index element={<HomePage />} />
-              <Route path="home" element={<HomePage />} />
-              <Route path="news" element={<NewsPage />} />
-              <Route path="notices" element={<NoticesPage />}>
-                <Route path="sell" element={<NoticesCategoriesList />} />
-                <Route path="lost-found" element={<NoticesCategoriesList />} />
-                <Route path="for-free" element={<NoticesCategoriesList />} />
-                <Route path="favorite" element={<NoticesCategoriesList />} />
-                <Route path="own" element={<NoticesCategoriesList />} />
-              </Route>
-              <Route path="friends" element={<OurFriendsPage />} />
-              <Route path="register" element={<RegisterPage />} />
-              <Route path="login" element={<LoginPage />} />
-              <Route path="user" element={<ProfilePage />} />
-              <Route path="*" element={<HomePage />} />
+            {/* <Route path="/" element={<SharedLayout />}> */}
+            <Route path="/" element={<HomePage />} />
+            <Route index element={<HomePage />} />
+            <Route path="home" element={<HomePage />} />
+            <Route path="news" element={<NewsPage />} />
+            <Route path="notices" element={<NoticesPage />}>
+              <Route path="sell" element={<NoticesCategoriesList />} />
+              <Route path="lost-found" element={<NoticesCategoriesList />} />
+              <Route path="for-free" element={<NoticesCategoriesList />} />
+              <Route path="favorite" element={<NoticesCategoriesList />} />
+              <Route path="own" element={<NoticesCategoriesList />} />
             </Route>
+            <Route path="friends" element={<OurFriendsPage />} />
+            <Route path="register" element={<RegisterPage />} />
+            <Route path="login" element={<LoginPage />} />
+            <Route path="user" element={<ProfilePage />} />
+            <Route path="*" element={<HomePage />} />
+            {/* </Route> */}
             <Route path="confirm-page" element={<ConfirmEmailPage />} />
           </Routes>
         </Container>
