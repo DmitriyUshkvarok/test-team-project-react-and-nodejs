d3cabb82d1655392a837a1683f4f4bdb1e05c047
DmitriyUshkvarok <dmitriy_ushkvarok@ukr.net>
2023-06-13 21:18:29 +0300

    ...fix route

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,10 +4,8 @@ import { Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import authOperation from '../../redux/auth/authOperation';
-// import SharedLayout from '../SharedLayout/SharedLayout';
+import SharedLayout from '../SharedLayout/SharedLayout';
 import Container from '../Container/Container';
-import NoticesCategoriesList from '../Notices/NoticesCategoriesList/NoticesCategoriesList';
-import Header from '../Header/Header';
 
 const HomePage = lazy(() => import('../../pages/HomePage/HomePage'));
 const NewsPage = lazy(() => import('../../pages/NewsPage/NewsPage'));
@@ -36,26 +34,17 @@ function App() {
     <>
       <Suspense fallback={<div>Loading subpage ....</div>}>
         <Container>
-          <Header />
           <Routes>
-            {/* <Route path="/" element={<SharedLayout />}> */}
-            <Route path="/" element={<HomePage />} />
-            <Route index element={<HomePage />} />
-            <Route path="home" element={<HomePage />} />
-            <Route path="news" element={<NewsPage />} />
-            <Route path="notices" element={<NoticesPage />}>
-              <Route path="sell" element={<NoticesCategoriesList />} />
-              <Route path="lost-found" element={<NoticesCategoriesList />} />
-              <Route path="for-free" element={<NoticesCategoriesList />} />
-              <Route path="favorite" element={<NoticesCategoriesList />} />
-              <Route path="own" element={<NoticesCategoriesList />} />
+            <Route path="/" element={<SharedLayout />}>
+              <Route path="home" element={<HomePage />} />
+              <Route path="news" element={<NewsPage />} />
+              <Route path="notices" element={<NoticesPage />} />
+              <Route path="friends" element={<OurFriendsPage />} />
+              <Route path="register" element={<RegisterPage />} />
+              <Route path="login" element={<LoginPage />} />
+              <Route path="user" element={<ProfilePage />} />
+              <Route path="*" element={<HomePage />} />
             </Route>
-            <Route path="friends" element={<OurFriendsPage />} />
-            <Route path="register" element={<RegisterPage />} />
-            <Route path="login" element={<LoginPage />} />
-            <Route path="user" element={<ProfilePage />} />
-            <Route path="*" element={<HomePage />} />
-            {/* </Route> */}
             <Route path="confirm-page" element={<ConfirmEmailPage />} />
           </Routes>
         </Container>
